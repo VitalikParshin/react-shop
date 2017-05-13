f849793bfc2f16f270b0b133385bc822894bd211
a.serga <a.serga@smartweb.com.ua>
2017-05-13 17:45:21 +0300

    FlatPages

--- src/modules/layout/FlatPages/FlatPages.tsx
+++ src/modules/layout/FlatPages/FlatPages.tsx
@@ -27,64 +27,68 @@ function createMarkup(html) {
   return {__html: html};
 }
 
+
 class FlatPages extends React.Component<any, any> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      modal1: false,
-      modal2: false,
-      modal3: false,
-      modal4: false,
-      modal5: false,
-      modal6: false,
-      modal7: false,
-      modal8: false,
-      modal9: false,
-      modal10: false,
-      modal11: false,
+
+  state = {
+    showModal: false,
+    page: {
+      name: "",
+      content: "",
     }
   }
 
-  showModal = key => (e) => {
-    e.preventDefault(); // 修复 Android 上点击穿透
+  showModal = (e, page) => {
+    e.preventDefault();
     this.setState({
-      [key]: true,
-    });
-  }
-  onClose = key => () => {
-    this.setState({
-      [key]: false,
+      page: page,
+      showModal: true,
     });
   }
 
+  closeModal = () => {
+    this.setState({
+      content: "",
+      showModal: false,
+    });
+  }
 
   render() {
     const { data }  = this.props;
-    const {loading, flatPages} = data;
+    const { loading, flatPages } = data;
 
     if (loading === true) {
       return <Loading/>;
     }
+
     return (
       <div>
-        {flatPages.slice(1).map(page => (
-            <div style={{marginLeft: "10px"}}>
-              <List>
-                <Button onClick={this.showModal('modal1')}>{page.name}</Button>
-                <Modal
-                  // title="Title Page"
-                  transparent
-                  maskClosable={false}
-                  visible={this.state.modal1}
-                  onClose={this.onClose('modal1')}
-                  footer={[{ text: 'Назад', onPress: () => { console.log('ok'); this.onClose('modal1')(); } }]}
-                >
-                  <div dangerouslySetInnerHTML={createMarkup(page.content)}></div>
-                </Modal>
-              </List>
-              <WhiteSpace />
-            </div>
-        ))}
+        <List>
+          {flatPages.map(page => (
+            <List.Item
+              arrow="horizontal"
+              thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+              onClick={(e) => this.showModal(e, page) }
+            >
+              {page.name}
+            </List.Item>
+          ))}
+        </List>
+
+        <Modal
+          closable
+          maskClosable
+          transparent={false}
+          title={this.state.page.name}
+          visible={this.state.showModal}
+          animationType="fade"
+          onClose={this.closeModal}
+        >
+          <div
+            dangerouslySetInnerHTML={createMarkup(this.state.page.content)}
+            style={{ textAlign: "left", margin: "30px 20px" }}
+          />
+        </Modal>
       </div>
     )
   }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { Flex, Button } from "antd-mobile";
-import { Catalog, FlatPages } from "../../modules/layout/index";
+import {Flex, Button, WhiteSpace} from "antd-mobile";
+import {Catalog, FlatPages} from "../../modules/layout/index";
 
 const HomePage = () => {
   window.addEventListener('scroll', function(){console.log('hEllo')})
   return (
     <div>
       <Catalog />
+      <WhiteSpace size="lg" />
       <FlatPages />
     </div>
   )
