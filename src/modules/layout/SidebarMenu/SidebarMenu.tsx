40fbef5234f43a44583565626717263bd8a2c341
a.serga <a.serga@smartweb.com.ua>
2017-05-17 19:04:19 +0300

    Add touch to SidebarMenu

--- src/modules/layout/SidebarMenu/SidebarMenu.tsx
+++ src/modules/layout/SidebarMenu/SidebarMenu.tsx
@@ -3,7 +3,7 @@ import {ACTION_TOOTLE_CATALOG, ACTION_TOOTLE_MENU} from "../constants";
 import Sidebar from "react-sidebar";
 import { gql, compose, graphql } from "react-apollo";
 import { connect } from "react-redux";
-import {Menu} from "../index";
+import {Menu, utils} from "../index";
 
 class SidebarMenu extends React.Component < any,
 any > {
@@ -18,7 +18,8 @@ any > {
 
     return (
       <Sidebar
-        touch={false}
+        touch={utils.swipeEnabled()}
+        touchHandleWidth={utils.swipeEnabled() ? 0 : undefined}
         sidebar={<Menu/>}
         open={layout.openMenu}
         onSetOpen={this.onSetSidebarOpen as any}
