15521b69186f36e25aed7f6b987ac53d90cb5c21
a.serga <a.serga@smartweb.com.ua>
2017-07-04 15:35:13 +0300

    cleanup

--- src/modules/layout/Layout/Layout.tsx
+++ src/modules/layout/Layout/Layout.tsx
@@ -1,22 +1,9 @@
-import { Button, Flex, Icon, List, WingBlank } from "antd-mobile";
 import * as React from "react";
-import {compose, graphql} from "react-apollo";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { ACTION_TOOTLE_CATALOG } from "../constants";
-import {HEIGHT} from "../Header/Header";
-import {Footer, Header, SidebarCatalog, SidebarMenu} from "../index";
+
+import { Header, SidebarCatalog, SidebarMenu } from "../index";
 
 class Layout extends React.Component<any, any> {
   render() {
-    // return (
-    //   <div>
-    //     <Header/>
-    //     <SidebarCatalog>
-    //       <div>{this.props.children}</div>
-    //     </SidebarCatalog>
-    //   </div>
-    // );
     return (
       <div>
         <Header/>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import WingBlank from "antd-mobile/lib/wing-blank/index.web";
 import * as React from "react";
 import { connect } from "react-redux";
 
-import { ACTION_RESET } from '../../modules/layout/constants';
+import { ACTION_RESET } from "../../modules/layout/constants";
 import { Catalog, FlatPages } from "../../modules/layout/index";
 
 class HomePage extends React.Component<any, any> {
